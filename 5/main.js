c247b4d966422ca8cec987045f37b55b3ee60d8e
Mark Agaton <markallenagaton03@gmail.com>
2020-06-06 18:02:04 +0800

    finished video 5

---
+++ 5/main.js
@@ -0,0 +1,24 @@
+const subject = new Rx.Subject();
+
+subject.subscribe({
+  next(value) {
+    console.log(value);
+  },
+  error(error) {
+    console.log(error);
+  },
+  complete() {
+    console.log('Completed')
+  }
+});
+
+subject.subscribe({
+  next(value) {
+    console.log(value);
+  }
+});
+
+subject.next('A new data piece');
+// subject.error('Error');
+subject.complete();
+subject.next('New value');
