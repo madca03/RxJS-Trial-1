fdd97d626bbd0e315d207874a62d1c43dfa5541e
Mark Agaton <markallenagaton03@gmail.com>
2020-06-06 21:45:20 +0800

    updated 11

--- 11/main.js
+++ 11/main.js
@@ -3,6 +3,13 @@ const button = document.querySelector('button');
 const obs1 = Rx.Observable.fromEvent(button, 'click');
 const obs2 = Rx.Observable.interval(1000);
 
+// clicking multiple times will not cancel old subscriptions
+// obs1.subscribe(
+//   (event) => obs2.subscribe(
+//     (value) => console.log(value)
+//   )
+// )
+
 obs1.switchMap(
   (event) => {
     return obs2
