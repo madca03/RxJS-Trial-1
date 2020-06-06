cbdd2c0d2e32e2c220e39a3cbe16dec3bb429917
Mark Agaton <markallenagaton03@gmail.com>
2020-06-06 18:23:03 +0800

    finished video 7

---
+++ 7/main.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('input');
+const observable = Rx.Observable.fromEvent(input, 'input');
+
+observable
+.map(event => event.target.value)
+.debounceTime(500)
+.distinctUntilChanged()
+.subscribe({
+  next(value) {
+    console.log(value);
+  }
+});
